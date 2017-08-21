a4f62eace6d6cb66cc2e7cbdf404645574ac4422
Kai Schnittcher <kai.schnittcher@gmail.com>
2017-08-21 12:53:48 +0200

    Add Switch (#4)

---
+++ HomeKitBridge/services/switch.php
@@ -0,0 +1,21 @@
+<?
+
+class HAPServiceSwitch extends HAPService {
+
+    public function __construct() {
+
+        parent::__construct(
+            0x49,
+            Array(
+                //Required Characteristics
+                new HAPCharacteristicOn()
+            ),
+            Array(
+                //Optional Characteristics
+                new HAPCharacteristicName()
+            )
+        );
+
+    }
+
+}
