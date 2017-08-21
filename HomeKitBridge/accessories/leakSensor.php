51126760ba771d1bde4d7b110674fd0b24780638
Kai Schnittcher <kai.schnittcher@gmail.com>
2017-08-21 16:42:30 +0200

    fix code style

--- HomeKitBridge/accessories/leakSensor.php
+++ HomeKitBridge/accessories/leakSensor.php
@@ -102,7 +102,7 @@ class HAPAccessoryConfigurationLeakSensor {
         $targetVariable = IPS_GetVariable($data["VariableID"]);
 
         if($targetVariable['VariableType'] != 1 /* Integer */) {
-          return "Integer required";
+            return "Integer required";
         }
 
         return "OK";
