c1469a14a14c66bb79df8f0072d10d00364758f9
Michael Maroszek <maroszek@gmx.net>
2017-08-23 13:43:54 +0200

    add more boilerplate for testing

---
+++ tests/HomeKitDiscoveryTest.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+include_once 'SymconGlobalStubs.php';
+include_once 'SymconModuleStubs.php';
+include_once '../HomeKitDiscovery/module.php';
+
+class HomeKitDiscoveryTest extends TestCase
+{
+    public function setUp()
+    {
+        //Register our library we need for testing
+        IPSKernel::loadLibrary('../library.json');
+
+        parent::setUp();
+    }
+
+    public function testCreate(): void {
+        $instance = new HomeKitDiscovery(0);
+        $instance->Create();
+        $form = json_decode($instance->GetConfigurationForParent(), true);
+
+        $this->assertEquals($form, [
+            'Host'               => '224.0.0.251',
+            'Port'               => 5353,
+            'BindPort'           => 5353,
+            'MulticastIP'        => '224.0.0.251',
+            'EnableBroadcast'    => true,
+            'EnableReuseAddress' => true,
+            'EnableLoopback'     => true
+        ]);
+    }
+}
---
+++ tests/HomeKitTest.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+include_once "SymconValidator.php";
+
+class HomeKitTest extends TestCaseSymconValidation
+{
+    public function testValidateHomeKit(): void
+    {
+        $this->validateLibrary('..');
+    }
+
+    public function testValidateHomeKitDiscovery(): void
+    {
+        $this->validateModule('../HomeKitDiscovery');
+    }
+
+    public function testValidateHomeKitBridge(): void
+    {
+        $this->validateModule('../HomeKitBridge');
+    }
+}
