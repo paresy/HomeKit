2f62eb4468fecf32e7864682ed20ec2a7c923cea
Kai Schnittcher <kai.schnittcher@gmail.com>
2018-03-26 23:25:03 +0200

    add air quality sensor

---
+++ HomeKitBridge/accessories/airQuailitySensor.php
@@ -0,0 +1,68 @@
+<?php
+
+declare(strict_types=1);
+
+class HAPAccessoryAirQualitySensor extends HAPAccessoryBase
+{
+    public function __construct($data)
+    {
+        parent::__construct(
+            $data,
+            [
+                new HAPServiceAccessoryInformation(),
+                new HAPServiceAirQualitySensor()
+            ]
+        );
+    }
+
+    public function readCharacteristicAirQuality()
+    {
+        return GetValue($this->data['VariableID']);
+
+    }
+}
+
+class HAPAccessoryConfigurationAirQualitySensor
+{
+    public static function getPosition()
+    {
+        return 70;
+    }
+
+    public static function getCaption()
+    {
+        return 'Air Quality Sensor';
+    }
+
+    public static function getColumns()
+    {
+        return [
+            [
+                'label' => 'VariableID',
+                'name'  => 'VariableID',
+                'width' => '150px',
+                'add'   => 0,
+                'edit'  => [
+                    'type' => 'SelectVariable'
+                ]
+            ]
+        ];
+    }
+
+    public static function getStatus($data)
+    {
+        if (!IPS_VariableExists($data['VariableID'])) {
+            return 'Variable missing';
+        }
+
+        $targetVariable = IPS_GetVariable($data['VariableID']);
+
+        if ($targetVariable['VariableType'] != 1 /* Integer */ && $targetVariable['VariableType'] != 2 /* Float */) {
+            return 'Int/Float required';
+        }
+
+        return 'OK';
+    }
+}
+
+HomeKitManager::registerAccessory('AirQualitySensor');
