6ad5c227a47f73aba40cfbc47972af15db9894b4
Michael Maroszek <maroszek@gmx.net>
2018-04-09 16:02:15 +0200

    add simple test using new server socket stubs

--- tests/HomeKitBridgeTest.php
+++ tests/HomeKitBridgeTest.php
@@ -8,19 +8,19 @@ class HomeKitBridgeTest extends HomeKitBaseTest
 {
     public function testCreate(): void
     {
-        $iid = IPS_CreateInstance($this->bridgeModuleID);
+        $bridgeID = IPS_CreateInstance($this->bridgeModuleID);
         $this->assertEquals(1, count(IPS_GetInstanceListByModuleID($this->bridgeModuleID)));
         $this->assertEquals(1, count(IPS_GetInstanceListByModuleID($this->serverModuleID)));
-        $this->assertEquals(IPS_GetInstanceListByModuleID($this->serverModuleID)[0], IPS_GetInstance($iid)['ConnectionID']);
+        $this->assertEquals(IPS_GetInstanceListByModuleID($this->serverModuleID)[0], IPS_GetInstance($bridgeID)['ConnectionID']);
     }
 
     public function testConfigurationForm(): void
     {
-        $iid = IPS_CreateInstance($this->bridgeModuleID);
-        $form = json_decode(IPS_GetConfigurationForParent($iid), true);
+        $bridgeID = IPS_CreateInstance($this->bridgeModuleID);
+        $form = json_decode(IPS_GetConfigurationForParent($bridgeID), true);
 
         $this->assertEquals([
-            'Port' => IPS_GetProperty($iid, 'BridgePort')
+            'Port' => IPS_GetProperty($bridgeID, 'BridgePort')
         ], $form);
     }
 
@@ -33,4 +33,46 @@ class HomeKitBridgeTest extends HomeKitBaseTest
         //Check if the generated content matches our test file
         $this->assertEquals(json_decode(file_get_contents(__DIR__ . '/Accessories/None.json'), true), $bridgeInterface->DebugAccessories());
     }
+
+    public function testSetupCode(): void
+    {
+        $bridgeID = IPS_CreateInstance($this->bridgeModuleID);
+
+        $bridgeInterface = IPS\InstanceManager::getInstanceInterface($bridgeID);
+
+        $setupCode = $bridgeInterface->RestartPairing();
+
+        $this->assertTrue(preg_match('/\d{3}-\d{2}-\d{3}/', $setupCode) == 1);
+    }
+
+    public function testHTTPInvalidResponse(): void
+    {
+        $bridgeID = IPS_CreateInstance($this->bridgeModuleID);
+
+        $bridgeInterface = IPS\InstanceManager::getInstanceInterface($bridgeID);
+
+        $setupCode = $bridgeInterface->RestartPairing();
+
+        $serverID = IPS_GetInstance($bridgeID)['ConnectionID'];
+
+        $serverInterface = IPS\InstanceManager::getInstanceInterface($serverID);
+
+        $clientIP = "127.0.0.1";
+        $clientPort = 34455;
+
+        $data = "Totally Invalid Packet\r\n\r\n\r\n";
+        $serverInterface->PushConnect($clientIP, $clientPort);
+        $serverInterface->PushPacket($data, $clientIP, $clientPort);
+
+        $this->assertTrue($serverInterface->HasPacket());
+
+        $this->assertEquals(array (
+            'Type' => 0 /* Data */,
+            'Buffer' => 'HTTP/1.1 500 Internal Server Error' . "\r\n\r\n",
+            'ClientIP' => $clientIP,
+            'ClientPort' => $clientPort,
+        ), $serverInterface->PeekPacket());
+
+    }
+
 }
