7ef50a78d21dc44483341766327966503fc1fd86
Kai Schnittcher <kai.schnittcher@gmail.com>
2018-04-10 10:42:59 +0200

    add WindowCovering

--- HomeKitBridge/accessories/windowCovering.php
+++ HomeKitBridge/accessories/windowCovering.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-class HAPAccessoryLightbulbSwitch extends HAPAccessoryBase
+class HAPAccessoryWindowCovering extends HAPAccessoryBase
 {
     public function __construct($data)
     {
@@ -10,19 +10,39 @@ class HAPAccessoryLightbulbSwitch extends HAPAccessoryBase
             $data,
             [
                 new HAPServiceAccessoryInformation(),
-                new HAPServiceLightbulb()
+                new HAPServiceWindowCovering()
             ]
         );
     }
 
-    public function readCharacteristicOn()
+    public function readCharacteristicTargetPosition()
     {
-        return GetValue($this->data['VariableID']);
+        return GetValue($this->data['TargetPosition']);
     }
 
-    public function writeCharacteristicOn($value)
+    public function readCharacteristicCurrentPosition()
     {
-        $this->switchDevice($this->data['VariableID'], $value);
+        return GetValue($this->data['CurrentPosition']);
+    }
+
+    public function readCharacteristicPositionState()
+    {
+        return GetValue($this->data['PositionState']);
+    }
+
+    public function writeCharacteristicTargetPosition($value)
+    {
+        $this->switchDevice($this->data['TargetPosition'], $value);
+    }
+
+    public function writeCharacteristicCurrentPosition($value)
+    {
+        $this->switchDevice($this->data['CurrentPosition'], $value);
+    }
+
+    public function writeCharacteristicPositionState($value)
+    {
+        $this->switchDevice($this->data['PositionState'], $value);
     }
 
     protected function switchDevice($variableID, $value)
@@ -61,24 +81,42 @@ class HAPAccessoryLightbulbSwitch extends HAPAccessoryBase
     }
 }
 
-class HAPAccessoryConfigurationLightbulbSwitch
+class HAPAccessoryConfigurationWindowCovering
 {
     public static function getPosition()
     {
-        return 1;
+        return 80;
     }
 
     public static function getCaption()
     {
-        return 'Lightbulb (Switch)';
+        return 'Window Covering';
     }
 
     public static function getColumns()
     {
         return [
             [
-                'label' => 'VariableID',
-                'name'  => 'VariableID',
+                'label' => 'TargetPosition',
+                'name'  => 'TargetPosition',
+                'width' => '150px',
+                'add'   => 0,
+                'edit'  => [
+                    'type' => 'SelectVariable'
+                ]
+            ],
+            [
+                'label' => 'CurrentPosition',
+                'name'  => 'CurrentPosition',
+                'width' => '150px',
+                'add'   => 0,
+                'edit'  => [
+                    'type' => 'SelectVariable'
+                ]
+            ],
+            [
+                'label' => 'PositionState',
+                'name'  => 'PositionState',
                 'width' => '150px',
                 'add'   => 0,
                 'edit'  => [
@@ -90,28 +128,46 @@ class HAPAccessoryConfigurationLightbulbSwitch
 
     public static function getStatus($data)
     {
-        if (!IPS_VariableExists($data['VariableID'])) {
-            return 'Variable missing';
+        if (!IPS_VariableExists($data['TargetPosition'])) {
+            return 'TargetPosition missing';
         }
 
-        $targetVariable = IPS_GetVariable($data['VariableID']);
-
-        if ($targetVariable['VariableType'] != 0 /* Boolean */) {
-            return 'Bool required';
+        if (!IPS_VariableExists($data['CurrentPosition'])) {
+            return 'CurrentPosition missing';
         }
 
-        if ($targetVariable['VariableCustomAction'] != '') {
-            $profileAction = $targetVariable['VariableCustomAction'];
+        if (!IPS_VariableExists($data['PositionState'])) {
+            return 'PositionState missing';
+        }
+
+        $variableTargetPosition = IPS_GetVariable($data['TargetPosition']);
+        $variableCurrentPosition = IPS_GetVariable($data['CurrentPosition']);
+        $variablePositionState = IPS_GetVariable($data['PositionState']);
+
+        if ($variableTargetPosition['VariableType'] != 1 /* Integer */) {
+            return 'TargetPosition: Integer required';
+        }
+
+        if ($variableCurrentPosition['VariableType'] != 1 /* Integer */) {
+            return 'CurrentPosition: Integer required';
+        }
+
+        if ($variablePositionState['VariableType'] != 1 /* Integer */) {
+            return 'PositionState: Integer required';
+        }
+
+        if ($variableTargetPosition['VariableCustomAction'] != '') {
+            $profileAction = $variableTargetPosition['VariableCustomAction'];
         } else {
-            $profileAction = $targetVariable['VariableAction'];
+            $profileAction = $variableTargetPosition['VariableAction'];
         }
 
         if (!($profileAction > 10000)) {
-            return 'Action required';
+            return 'TargetDoorState: Action required';
         }
 
         return 'OK';
     }
 }
 
-HomeKitManager::registerAccessory('LightbulbSwitch');
+HomeKitManager::registerAccessory('WindowCovering');
