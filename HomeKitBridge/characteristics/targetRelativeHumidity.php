41ab12c3eb3ff03b7bb7e966e4d99c3805ce7903
Kai Schnittcher <kai.schnittcher@gmail.com>
2018-04-10 11:55:51 +0200

    fix HAPCharacteristicTargetRelativeHumidity - wrong class name

--- HomeKitBridge/characteristics/targetRelativeHumidity.php
+++ HomeKitBridge/characteristics/targetRelativeHumidity.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-class HAPCharacteristicRelativeHumidity extends HAPCharacteristic
+class HAPCharacteristicTargetRelativeHumidity extends HAPCharacteristic
 {
     public function __construct()
     {
