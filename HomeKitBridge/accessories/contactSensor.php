4888cc1f7405207abb2f75835f61ba056309d591
mvs90 <39708178+mvs90@users.noreply.github.com>
2018-06-02 19:16:22 +0200

    Update contactSensor.php

--- HomeKitBridge/accessories/contactSensor.php
+++ HomeKitBridge/accessories/contactSensor.php
@@ -14,14 +14,14 @@ class HAPAccessoryContactSensor extends HAPAccessoryBase
             ]
         );
     }
-
+	
     public function notifyCharacteristicContactSensorState()
     {
         return [
             $this->data['VariableID']
         ];
     }
-
+	
     public function readCharacteristicContactSensorState()
     {
         switch (GetValue($this->data['VariableID'])) {
@@ -40,12 +40,12 @@ class HAPAccessoryConfigurationContactSensor
     {
         return 90;
     }
-
+	
     public static function getCaption()
     {
         return 'Contact Sensor';
     }
-
+	
     public static function getColumns()
     {
         return [
@@ -60,16 +60,15 @@ class HAPAccessoryConfigurationContactSensor
             ]
         ];
     }
-
+	
     public static function getStatus($data)
     {
         if (!IPS_VariableExists($data['VariableID'])) {
             return 'Variable missing';
         }
-
         return 'OK';
     }
-
+	
     public static function getTranslations()
     {
         return [
