2aa6e1b1aa7b40a0661e3ada06992ef40c9657ad
Kai Schnittcher <kai.schnittcher@gmail.com>
2018-07-23 14:55:50 +0200

    add CarbonDioxideSensor

---
+++ HomeKitBridge/accessories/carbonDioxideSensor.php
@@ -0,0 +1,106 @@
+<?php
+
+declare(strict_types=1);
+
+class HAPAccessoryCarbonDioxideSensor extends HAPAccessoryBase
+{
+    public function __construct($data)
+    {
+        parent::__construct(
+            $data,
+            [
+                new HAPServiceAccessoryInformation(),
+                new HAPServiceCarbonDioxideSensor()
+            ]
+        );
+    }
+
+    public function notifyCharacteristicCarbonDioxideDetected()
+    {
+        return [
+            $this->data['VariableID']
+        ];
+    }
+
+    public function readCharacteristicCarbonDioxideDetected()
+    {
+        $targetVariable = IPS_GetVariable($this->data['VariableID']);
+
+        if ($targetVariable['VariableCustomProfile'] != '') {
+            $profileName = $targetVariable['VariableCustomProfile'];
+        } else {
+            $profileName = $targetVariable['VariableProfile'];
+        }
+
+        $value = GetValue($this->data['VariableID']);
+
+        //invert value if the variable profile is inverted
+        if (strpos($profileName, '.Reversed') !== false) {
+            $value = !$value;
+        }
+
+        if ($value) {
+            return HAPCharacteristicCarbonDioxideDetected::Abnormal;
+        } else {
+            return HAPCharacteristicCarbonDioxideDetected::Normal;
+        }
+    }
+}
+
+class HAPAccessoryConfigurationCarbonDioxideSensor
+{
+    public static function getPosition()
+    {
+        return 150;
+    }
+
+    public static function getCaption()
+    {
+        return 'Carbon Dioxide Sensor';
+    }
+
+    public static function getColumns()
+    {
+        return [
+            [
+                'label' => 'VariableID',
+                'name'  => 'VariableID',
+                'width' => '250px',
+                'add'   => 0,
+                'edit'  => [
+                    'type' => 'SelectVariable'
+                ]
+            ]
+        ];
+    }
+
+    public static function getStatus($data)
+    {
+        if (!IPS_VariableExists($data['VariableID'])) {
+            return 'Variable missing';
+        }
+
+        $targetVariable = IPS_GetVariable($data['VariableID']);
+
+        if ($targetVariable['VariableType'] != 0 /* Boolean */) {
+            return 'Bool required';
+        }
+
+        return 'OK';
+    }
+
+    public static function getTranslations()
+    {
+        return [
+            'de' => [
+                'Carbon Dioxide Sensor'    => 'Kohlendioxid Sensor',
+                'VariableID'                => 'VariablenID',
+                'Variable missing'          => 'Variable fehlt',
+                'Bool required'             => 'Bool benötigt',
+                'OK'                        => 'OK'
+            ]
+        ];
+    }
+}
+
+HomeKitManager::registerAccessory('CarbonDioxideSensor');
---
+++ tests/HomeKitCarbonDioxideSensorTest.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+include_once __DIR__ . '/HomeKitBaseTest.php';
+
+class HomeKitCarbonDioxideSensorTest extends HomeKitBaseTest
+{
+    public function testAccessory(): void
+    {
+        $bridgeID = IPS_CreateInstance($this->bridgeModuleID);
+
+        $vid = IPS_CreateVariable(0 /* Boolean */);
+
+        IPS_SetProperty($bridgeID, 'AccessoryCarbonDioxideSensor', json_encode([
+            [
+                'ID'         => 3,
+                'Name'       => 'Test',
+                'VariableID' => $vid
+            ]
+        ]));
+        IPS_ApplyChanges($bridgeID);
+
+        $bridgeInterface = IPS\InstanceManager::getInstanceInterface($bridgeID);
+
+        $base = json_decode(file_get_contents(__DIR__ . '/exports/None.json'), true);
+        $accessory = json_decode(file_get_contents(__DIR__ . '/exports/CarbonDioxideSensor.json'), true);
+
+        //Check if the generated content matches our test file
+        $this->assertEquals(array_merge($base, $accessory), $bridgeInterface->DebugAccessories());
+    }
+
+    public function testAccessoryBroken(): void
+    {
+        $bridgeID = IPS_CreateInstance($this->bridgeModuleID);
+
+        IPS_SetProperty($bridgeID, 'AccessoryCarbonDioxideSensor', json_encode([
+            [
+                'ID'         => 3,
+                'Name'       => 'Test',
+                'VariableID' => 9999 /* This is always an invalid variableID */
+            ]
+        ]));
+        IPS_ApplyChanges($bridgeID);
+
+        $bridgeInterface = IPS\InstanceManager::getInstanceInterface($bridgeID);
+
+        //Check if the generated content matches our test file
+        $this->assertEquals(json_decode(file_get_contents(__DIR__ . '/exports/None.json'), true), $bridgeInterface->DebugAccessories());
+    }
+}
