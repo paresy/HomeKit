40bb6578ada0141b919d796b8872b7e209cd3e19
Kai Schnittcher <kai.schnittcher@gmail.com>
2018-11-21 14:53:10 +0100

    Thermostat hinzugefügt

---
+++ HomeKitBridge/accessories/thermostat.php
@@ -0,0 +1,180 @@
+<?php
+
+declare(strict_types=1);
+
+class HAPAccessoryThermostat extends HAPAccessoryBase
+{
+    use HelperSetDevice;
+
+    public function __construct($data)
+    {
+        parent::__construct(
+            $data,
+            [
+                new HAPServiceAccessoryInformation(),
+                new HAPServiceThermostat()
+            ]
+        );
+    }
+
+    public function notifyCharacteristicCurrentHeatingCoolingState()
+    {
+        return [
+            $this->data['CurrentHeatingCoolingStateID']
+        ];
+    }
+
+    public function notifyCharacteristicTargetHeatingCoolingState()
+    {
+        return [
+            $this->data['TargetHeatingCoolingStateID']
+        ];
+    }
+
+    public function notifyCharacteristicCurrentTemperature()
+    {
+        return [
+            $this->data['CurrentTemperatureID']
+        ];
+    }
+
+    public function notifyCharacteristicTemperatureDisplayUnits()
+    {
+        return [
+            HAPCharacteristicTemperatureDisplayUnits::Celsius
+        ];
+    }
+
+    public function readCharacteristicCurrentHeatingCoolingState()
+    {
+        return GetValue($this->data['CurrentHeatingCoolingStateID']);
+    }
+
+    public function readCharacteristicTargetHeatingCoolingState()
+    {
+        return GetValue($this->data['TargetHeatingCoolingStateID']);
+    }
+
+    public function readCharacteristicCurrentTemperature()
+    {
+        return GetValue($this->data['CurrentTemperatureID']);
+    }
+
+    public function readCharacteristicTemperatureDisplayUnits()
+    {
+        return HAPCharacteristicTemperatureDisplayUnits::Celsius;
+    }
+
+    public function writeCharacteristicTargetHeatingCoolingState($value)
+    {
+        self::setDevice($this->data['TargetHeatingCoolingStateID'], $value);
+    }
+
+    public function writeCharacteristicTemperatureDisplayUnits($value)
+    {
+        return;
+    }
+}
+
+class HAPAccessoryConfigurationThermostat
+{
+    use HelperSetDevice;
+
+    public static function getPosition()
+    {
+        return 260;
+    }
+
+    public static function getCaption()
+    {
+        return 'Thermostat';
+    }
+
+    public static function getColumns()
+    {
+        return [
+            [
+                'label' => 'CurrentHeatingCoolingStateID',
+                'name'  => 'CurrentHeatingCoolingStateID',
+                'width' => '250px',
+                'add'   => 0,
+                'edit'  => [
+                    'type' => 'SelectVariable'
+                ]
+            ],
+            [
+                'label' => 'TargetHeatingCoolingStateID',
+                'name'  => 'TargetHeatingCoolingStateID',
+                'width' => '250px',
+                'add'   => 0,
+                'edit'  => [
+                    'type' => 'SelectVariable'
+                ]
+            ],
+            [
+                'label' => 'CurrentTemperatureID',
+                'name'  => 'CurrentTemperatureID',
+                'width' => '250px',
+                'add'   => 0,
+                'edit'  => [
+                    'type' => 'SelectVariable'
+                ]
+            ]
+        ];
+    }
+
+    public static function getStatus($data)
+    {
+        if (!IPS_VariableExists($data['CurrentHeatingCoolingStateID'])) {
+            return 'Variable CurrentHeatingCoolingStateID missing';
+        }
+
+        if (!IPS_VariableExists($data['TargetHeatingCoolingStateID'])) {
+            return 'Variable TargetHeatingCoolingStateID missing';
+        }
+
+        if (!IPS_VariableExists($data['CurrentTemperatureID'])) {
+            return 'Variable CurrentTemperatureID missing';
+        }
+
+        $targetVariable = IPS_GetVariable($data['TargetHeatingCoolingStateID']);
+
+        if ($targetVariable['VariableType'] != 1 /* Integer */ ) {
+            return 'TargetHeatingCoolingStateID: Int required';
+        }
+
+        $targetVariable = IPS_GetVariable($data['CurrentHeatingCoolingStateID']);
+
+        if ($targetVariable['VariableType'] != 1 /* Integer */ ) {
+            return 'CurrentHeatingCoolingStateID: Int required';
+        }
+
+        $targetVariable = IPS_GetVariable($data['CurrentTemperatureID']);
+
+        if ($targetVariable['VariableType'] != 2 /* Float */ ) {
+            return 'CurrentTemperatureID: Float required';
+        }
+
+        return 'OK';
+    }
+
+    public static function getTranslations()
+    {
+        return [
+            'de' => [
+                'Thermostat'               => 'Thermostat',
+                'CurrentHeatingCoolingStateID'                => 'Aktueller Heizungsstatus (ID)',
+                'TargetHeatingCoolingStateID'              => 'Ziel Heizungsstatus (ID)',
+                'CurrentTemperatureID'      => 'Aktuelle Temperatur (ID) ',
+                'Variable CurrentHeatingCoolingStateID missing'         => 'Variable CurrentHeatingCoolingStateID fehlt',
+                'Variable CurrentTemperatureID missing'       => 'Variable CurrentTemperatureID fehlt',
+                'TargetHeatingCoolingStateID: Int required' => 'TargetHeatingCoolingStateID: Int benötigt',
+                'CurrentHeatingCoolingStateID: Int required' => 'CurrentHeatingCoolingStateID: Int benötigt',
+                'CurrentTemperatureID: Float required' => 'CurrentTemperatureID: Float benötigt',
+                'OK'                    => 'OK'
+            ]
+        ];
+    }
+}
+
+HomeKitManager::registerAccessory('Thermostat');
---
+++ tests/HomeKitThermostatTest.php
@@ -0,0 +1,65 @@
+<?php
+
+declare(strict_types=1);
+
+include_once __DIR__ . '/HomeKitBaseTest.php';
+
+class HomeKitThermostatTest extends HomeKitBaseTest
+{
+    public function testAccessory(): void
+    {
+        $bridgeID = IPS_CreateInstance($this->bridgeModuleID);
+
+        $chid = IPS_CreateVariable(0 /* Integer */); //CurrentHeatingCoolingStateID
+
+        $thid = IPS_CreateVariable(1 /* Integer */); //TargetHeatingCoolingStateID
+        IPS_SetVariableCustomAction($thid, 10001); //Any valid ID will do
+
+        $ctid = IPS_CreateVariable(2 /* Float */); //CurrentTemperatureID
+
+        IPS_SetProperty($bridgeID, 'AccessoryThermostat', json_encode([
+            [
+                'ID'         => 2,
+                'Name'       => 'Test Thermostat',
+                'CurrentHeatingCoolingStateID' => $chid,
+                'TargetHeatingCoolingStateID'   => $thid,
+                'CurrentTemperatureID'  => $ctid,
+            ]
+        ]));
+
+
+        IPS_ApplyChanges($bridgeID);
+
+        $bridgeInterface = IPS\InstanceManager::getInstanceInterface($bridgeID);
+
+        $base = json_decode(file_get_contents(__DIR__ . '/exports/None.json'), true);
+        $accessory = json_decode(file_get_contents(__DIR__ . '/exports/Thermostat.json'), true);
+
+        //Check if the generated content matches our test file
+        echo json_encode($bridgeInterface->DebugAccessories());
+        $this->assertEquals(array_merge($base, $accessory), $bridgeInterface->DebugAccessories());
+
+    }
+
+    public function testAccessoryBroken(): void
+    {
+        $bridgeID = IPS_CreateInstance($this->bridgeModuleID);
+
+
+        IPS_SetProperty($bridgeID, 'AccessoryThermostat', json_encode([
+            [
+                'ID'         => 2,
+                'Name'       => 'Test Thermostat',
+                'CurrentHeatingCoolingStateID'     => 9999,  /* This is always an invalid variableID */
+                'TargetHeatingCoolingStateID'   => 9999,  /* This is always an invalid variableID */
+                'CurrentTemperatureID' => 9999  /* This is always an invalid variableID */
+            ],
+        ]));
+        IPS_ApplyChanges($bridgeID);
+
+        $bridgeInterface = IPS\InstanceManager::getInstanceInterface($bridgeID);
+
+        //Check if the generated content matches our test file
+        $this->assertEquals(json_decode(file_get_contents(__DIR__ . '/exports/None.json'), true), $bridgeInterface->DebugAccessories());
+    }
+}
