59a70bd997104b289da5f4dd631eef5e3876331c
Kai Schnittcher <kai.schnittcher@gmail.com>
2019-08-23 20:47:39 +0200

    Add StatelessProgrammableSwitch

---
+++ HomeKitBridge/accessories/statelessProgrammableSwitch.php
@@ -0,0 +1,89 @@
+<?php
+
+declare(strict_types=1);
+
+class HAPAccessoryStatelessProgrammableSwitch extends HAPAccessoryBase
+{
+    public function __construct($data)
+    {
+        parent::__construct(
+            $data,
+            [
+                new HAPServiceAccessoryInformation(),
+                new HAPServiceStatelessProgrammableSwitch()
+            ]
+        );
+    }
+
+    public function notifyCharacteristicProgrammableSwitchEvent()
+    {
+        return [
+            $this->data['VariableID']
+        ];
+    }
+
+    public function readCharacteristicProgrammableSwitchEvent()
+    {
+        return GetValue($this->data['VariableID']);
+    }
+
+}
+
+class HAPAccessoryConfigurationStatelessProgrammableSwitch
+{
+    use HelperSetDevice;
+
+    public static function getPosition()
+    {
+        return 300;
+    }
+
+    public static function getCaption()
+    {
+        return 'Stateless Programmable Switch';
+    }
+
+    public static function getColumns()
+    {
+        return [
+            [
+                'label' => 'VariableID',
+                'name'  => 'VariableID',
+                'width' => '250px',
+                'add'   => 0,
+                'edit'  => [
+                    'type' => 'SelectVariable'
+                ]
+            ]
+        ];
+    }
+
+    public static function getStatus($data)
+    {
+        if (!IPS_VariableExists($data['VariableID'])) {
+            return 'Variable missing';
+        }
+
+        $targetVariable = IPS_GetVariable($data['VariableID']);
+
+        if ($targetVariable['VariableType'] != 1 /* Integer */) {
+            return 'Int required';
+        }
+        return 'OK';
+    }
+
+    public static function getTranslations()
+    {
+        return [
+            'de' => [
+                'Stateless Programmable Switch'                => 'Zustandsloser programmierbarer Schalter',
+                'VariableID'                                   => 'VariablenID',
+                'Variable missing'                             => 'Variable fehlt',
+                'Int required'                                 => 'Int benötigt',
+                'OK'                                           => 'OK'
+            ]
+        ];
+    }
+}
+
+HomeKitManager::registerAccessory('StatelessProgrammableSwitch');
---
+++ tests/HomeKitStatelessProgrammableSwitchTest.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+include_once __DIR__ . '/HomeKitBaseTest.php';
+
+class HomeKitStatelessProgrammableSwitchTest extends HomeKitBaseTest
+{
+    public function testAccessory(): void
+    {
+        $bridgeID = IPS_CreateInstance($this->bridgeModuleID);
+
+        $vid = IPS_CreateVariable(1 /* Integer */);
+
+        IPS_SetProperty($bridgeID, 'AccessoryStatelessProgrammableSwitch', json_encode([
+            [
+                'ID'         => 3,
+                'Name'       => 'Test',
+                'VariableID' => $vid
+            ]
+        ]));
+        IPS_ApplyChanges($bridgeID);
+
+        $bridgeInterface = IPS\InstanceManager::getInstanceInterface($bridgeID);
+
+        $base = json_decode(file_get_contents(__DIR__ . '/exports/None.json'), true);
+        $accessory = json_decode(file_get_contents(__DIR__ . '/exports/AccessoryStatelessProgrammableSwitch.json'), true);
+
+        //Check if the generated content matches our test file
+        $this->assertEquals(array_merge($base, $accessory), $bridgeInterface->DebugAccessories());
+    }
+
+    public function testAccessoryInvalidValue(): void
+    {
+        $bridgeID = IPS_CreateInstance($this->bridgeModuleID);
+
+        $vid = IPS_CreateVariable(2 /* Float */);
+
+        SetValue($vid, -5);
+
+        IPS_SetProperty($bridgeID, 'AccessoryTemperatureSensor', json_encode([
+            [
+                'ID'         => 3,
+                'Name'       => 'Test',
+                'VariableID' => $vid
+            ]
+        ]));
+        IPS_ApplyChanges($bridgeID);
+
+        $bridgeInterface = IPS\InstanceManager::getInstanceInterface($bridgeID);
+
+        $base = json_decode(file_get_contents(__DIR__ . '/exports/None.json'), true);
+        $accessory = json_decode(file_get_contents(__DIR__ . '/exports/AccessoryStatelessProgrammableSwitch.json'), true);
+
+        //Check if the generated content matches our test file
+        $this->assertEquals(array_merge($base, $accessory), $bridgeInterface->DebugAccessories());
+    }
+}
