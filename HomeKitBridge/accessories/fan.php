a124b079dde4f1ad8fcc947b91d6a16df90635ee
franklinvv <franklinvv@yoki.org>
2020-03-18 11:51:53 +0100

    Added fan accessory (#87)

---
+++ HomeKitBridge/accessories/fan.php
@@ -0,0 +1,115 @@
+<?php
+
+declare(strict_types=1);
+
+class HAPAccessoryFan extends HAPAccessoryBase
+{
+    use HelperDimDevice;
+
+    public function __construct($data)
+    {
+        parent::__construct(
+            $data,
+            [
+                new HAPServiceAccessoryInformation(),
+                new HAPServiceFan()
+            ]
+        );
+    }
+
+    public function notifyCharacteristicOn()
+    {
+        return [
+            $this->data['VariableID']
+        ];
+    }
+
+    public function readCharacteristicOn()
+    {
+        return self::getDimValue($this->data['VariableID']) > 0;
+    }
+
+    public function writeCharacteristicOn($value)
+    {
+        //Only switch the device on, if it isn't on.
+        //This should fix the problem that Apple sends on before dimming
+        if ($value && $this->readCharacteristicOn()) {
+            return;
+        }
+
+        if ($value) {
+            self::dimDevice($this->data['VariableID'], 100);
+        } else {
+            self::dimDevice($this->data['VariableID'], 0);
+        }
+    }
+
+    public function notifyCharacteristicRotationSpeed()
+    {
+        return [
+            $this->data['VariableID']
+        ];
+    }
+
+    public function readCharacteristicRotationSpeed()
+    {
+        return self::getDimValue($this->data['VariableID']);
+    }
+
+    public function writeCharacteristicRotationSpeed($value)
+    {
+        self::dimDevice($this->data['VariableID'], $value);
+    }
+}
+
+class HAPAccessoryConfigurationFan
+{
+    use HelperDimDevice;
+
+    public static function getPosition()
+    {
+        return 10;
+    }
+
+    public static function getCaption()
+    {
+        return 'Fan';
+    }
+
+    public static function getColumns()
+    {
+        return [
+            [
+                'label' => 'VariableID',
+                'name'  => 'VariableID',
+                'width' => '250px',
+                'add'   => 0,
+                'edit'  => [
+                    'type' => 'SelectVariable'
+                ]
+            ]
+        ];
+    }
+
+    public static function getStatus($data)
+    {
+        return self::getDimCompatibility($data['VariableID']);
+    }
+
+    public static function getTranslations()
+    {
+        return [
+            'de' => [
+                'Fan'                   => 'Lüfter',
+                'VariableID'            => 'VariablenID',
+                'Variable missing'      => 'Variable fehlt',
+                'Int/Float required'    => 'Int/Float benötigt',
+                'Profile required'      => 'Profil benötigt',
+                'Action required'       => 'Aktion benötigt',
+                'OK'                    => 'OK'
+            ]
+        ];
+    }
+}
+
+HomeKitManager::registerAccessory('Fan');
---
+++ tests/HomeKitFanTest.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+include_once __DIR__ . '/HomeKitBaseTest.php';
+
+class HomeKitFanTest extends HomeKitBaseTest
+{
+    public function testAccessory(): void
+    {
+        $bridgeID = IPS_CreateInstance($this->bridgeModuleID);
+
+        $VariableID = IPS_CreateVariable(1 /* Integer */);
+
+        //Currently stubs do not provide default profiles
+        if (!IPS_VariableProfileExists('~Intensity.100')) {
+            IPS_CreateVariableProfile('~Intensity.100', 1 /* Integer */);
+            IPS_SetVariableProfileValues('~Intensity.100', 0, 100, 1);
+        }
+
+        IPS_SetVariableCustomProfile($VariableID, '~Intensity.100');
+        IPS_SetVariableCustomAction($VariableID, 10001); //Any valid ID will do
+
+        IPS_SetProperty($bridgeID, 'AccessoryFan', json_encode([
+            [
+                'ID'                    => 3,
+                'Name'                  => 'Test',
+                'VariableID'            => $VariableID
+            ]
+        ]));
+        IPS_ApplyChanges($bridgeID);
+
+        $bridgeInterface = IPS\InstanceManager::getInstanceInterface($bridgeID);
+
+        $base = json_decode(file_get_contents(__DIR__ . '/exports/None.json'), true);
+        $accessory = json_decode(file_get_contents(__DIR__ . '/exports/Fan.json'), true);
+
+        //Check if the generated content matches our test file
+        $this->assertEquals(array_merge($base, $accessory), $bridgeInterface->DebugAccessories());
+    }
+}
